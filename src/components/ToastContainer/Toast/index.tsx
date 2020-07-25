c43f9d344d6fbfe00e17c4673101eb7b852240ca
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-07-25 16:08:31 -0300

    Tempo para fechar o Toast

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import React, { useEffect } from 'react';
+import {
+  FiAlertCircle,
+  FiCheckCircle,
+  FiInfo,
+  FiXCircle,
+} from 'react-icons/fi';
 
 import { Container } from './styles';
 import { ToastMessage, useToast } from '../../../context/ToastContext';
 
+// A interface foi criada reaproveitando a interface que está no index do ToastContext
 interface ToastProps {
   message: ToastMessage;
 }
 
+// Criando um objeto para passar os ícones de acordo com o tipo de alerta.
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+};
+
 const Toast: React.FC<ToastProps> = ({ message }) => {
   const { removeToast } = useToast();
 
+  // Passando um efeito para executar toda vez que o Toast surgir na tela, colocamos então um tempo para ele ficar visível, depois o removemos.
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    // Caso o toast seja removido antes dos 3 segundo pelo usuário nós cancelamos o Timer. Então utilizamos a teoria do React, que diz que toda vez que não tiver mais um objeto, uma função é automáticamente executada
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message.id, removeToast]);
+
   return (
     <Container type={message.type} hasDescription={!!message.description}>
-      <FiAlertCircle size={20} />
+      {/* Trazendo o ícone de acordo com o tipo, como não é obrigatória, passamos info caso não exista o tipo */}
+      {icons[message.type || 'info']}
 
       <div>
         <strong>{message.title}</strong>
